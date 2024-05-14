87213476afc4f489a23a8a54fa0117a927327c97
titus kipngeno <tkipngeno@safaricom.co.ke>
2024-05-14 21:43:00 +0300

    add url

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({  
-  base: '/multi-step-form-vite-react/',
+export default defineConfig({
+  base: '/register/',
   plugins: [react()],
 })
